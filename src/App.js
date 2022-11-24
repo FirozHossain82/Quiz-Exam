728a29ac487fb3b231da3f8002b26ea350b2dea2
FirozHossain82 <firozhossain295@gmail.com>
2022-11-24 16:21:28 +0600

    header section commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
 import './App.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Main from './Layout/Main';
+import Home from './Components/Home/Home';
+import Statistics from './Components/Statistics/Statistics';
+import Blog from './Components/Blog/Blog';
+import QuizDetaills from './Components/QuizDetails/QuizDetaills';
+import NotFound from './Components/NotFound/NotFound';
 
 function App() {
   const router = createBrowserRouter([
     {
       path: '/',
-      element: ''
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>
+        },
+        {
+          path: '/home',
+          element:  <Home></Home>
+        },
+        {
+          path: '/statistics',
+          element: <Statistics></Statistics>
+        },
+        {
+          path: '/blogs',
+          element: <Blog></Blog>
+        },
+        {
+          path: '/quizDetails',
+          element:<QuizDetaills></QuizDetaills>
+        },
+        {
+          path: '/*',
+          element: <NotFound></NotFound>
+        }
+      ]
     }
   ])
   return (
---
+++ src/Components/QuizDetails/QuizDetaills.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const QuizDetaills = () => {
+    return (
+        <div>
+            <h2>Quiz Details</h2>
+        </div>
+    );
+};
+
+export default QuizDetaills;
