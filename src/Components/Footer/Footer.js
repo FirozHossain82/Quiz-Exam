711361f698d93ea71ae00e98edf125abdc221f13
FirozHossain82 <firozhossain295@gmail.com>
2022-11-25 12:58:39 +0600

    barchart

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            <h2>This is Footer</h2>
+        <div className='bg-sky-300'>
+            
         </div>
     );
 };
